12613279fb98deaa222b8480f7bf3ac542ad2c1f
Jordan Bonnot <jojobonnot@gmail.com>
2019-02-14 09:00:59 +0100

    Controller

---
+++ ColorPicker/ColorPickerViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ColorPickerViewController.swift
+//  ColorPicker
+//
+//  Created by lpiem on 24/01/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerViewController: UIViewController {
+
+    var delegate: ColorPickerViewDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+
+}
+
+protocol ColorPickerViewDelegate {
+    func userDidChooseColor(color: UIColor)
+}
--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "pickColor"){
+            let delegateVC = segue.destination as! ColorPickerViewController
+            delegateVC.delegate = self
+    
+        }
+    }
+
+    
+    
 
 
+}
+
+extension ViewController : ColorPickerViewDelegate {
+    func userDidChooseColor(color: UIColor) {
+        dismiss(animated: false, completion: nil)
+    }
 }
 
